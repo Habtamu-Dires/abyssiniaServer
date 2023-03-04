ef1e5b34f61cebb3c295e276f6343325b3f1171f
Habtamu <habtamuadugna.d@gmail.com>
2023-03-04 10:28:59 +0300

    security update 2

--- cloudinaryConfig.js
+++ cloudinaryConfig.js
@@ -18,16 +18,10 @@ module.exports = cloudinaryConfig;
 */
 const cloudinary = require('cloudinary').v2;
 
-const CLOUDINARY_CLOUD_NAME = 'dqlz3vcea';
-const CLOUDINARY_API_KEY = '682723734294982';
-const CLOUDINARY_API_SECRET = 'cYyOtpZUjl5IxuhOitPnPaoouFY';
-
-
 cloudinary.config({
-    cloud_name :  CLOUDINARY_CLOUD_NAME, //process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY, //process.env.CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET, //process.env.CLOUDINARY_API_SECRET,
-    //secure: true
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 module.exports = cloudinary;
